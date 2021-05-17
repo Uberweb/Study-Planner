feda944b5a67e38e5bbe9e800cfe17bb58ead48b
lizelim <limr@macstudent.com.au>
2021-05-17 20:42:18 +1000

    removed lists in the button added placeholders inside the inputs added verification text to sign-in and register fixed background image added title styles added json file fixed elements hiding and appearing for register

--- Script.js
+++ Script.js
@@ -1,7 +1,31 @@
-function Sign_in() {
-
-}
-
 function Register() {
+    let RegisterMenu = document.getElementById("RegisterMenu")
+    RegisterMenu.style.display = "inline-block"
+    let SignIn_RegisterMenu = document.getElementById("Sign-in/Register")
+    SignIn_RegisterMenu.style.display = "none"
+    let verify = false
+    while (verify === false) {
+        let Password = document.getElementById('R_Password')
+        let Username = document.getElementById('R_Username')
+        VerifyPassword(Password)
+            if (VerifyP === false) {
+                let V_Password = document.getElementById("V_Password")
+                V_Password.style.display = "inline-block"
+            } VerifyUsername(Username)
+        if (VerifyU === false) {
+
+        }
+}
+function VerifyPassword(Password) {
+    let VerifyP = false
+    if (length(Password) >= 6) {
+        VerifyP = true
+        return VerifyP
+    }
+}
+
+function VerifyUsername(Username) {
 
 }
+
+function Sign_in() {}
