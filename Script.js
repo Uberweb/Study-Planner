a3a460c738ea47cf0c160f14e02bd40b4dce7073
lizelim <limr@macstudent.com.au>
2021-07-19 22:42:00 +1000

    added comments throughout the javascript to add clarity in the function of the cody

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 //Time index: index is the day of the week index 0 = monday to 6 = sunday
-let UserPass = {Users: [
+let UserPass = {Users: [ //javascript object to store information about each user
         {Username: "test1", Password: "123", Subjects: ["English Advance", "Maths Advance", "Maths Extension 1", "Physics", "SDD", "IPT"],
             Assessments: ["SDD", "math_adv", "math_ext1"], Time: [4, 4, 3, 5, 4, 3, 4]},
         {Username: "test2", Password: "321", Subjects: ["English Advance", "English Extension 1", "English Extension 2", "Modern History", "Business Studies", "Art"],
@@ -9,40 +9,40 @@ let UserPass = {Users: [
 console.log(UserPass);
 
 function Register() {
-    let RegisterMenu = document.getElementById("RegisterMenu");
+    let RegisterMenu = document.getElementById("RegisterMenu"); //displaying the register elements
     RegisterMenu.style.display = "inline-block";
-    let SignIn_RegisterMenu = document.getElementById("Sign-in/Register");
+    let SignIn_RegisterMenu = document.getElementById("Sign-in/Register"); //hiding the sign-in or register elements
     SignIn_RegisterMenu.style.display = "none";
-    let SignInMenu = document.getElementById("Sign-inMenu");
+    let SignInMenu = document.getElementById("Sign-inMenu"); //hiding the sign-in menu elements
     SignInMenu.style.display = "none"
 }
 
 function R_Verification() {
-    let Password = document.getElementById('R_Password');
-    let Username = document.getElementById('R_Username');
+    let Password = document.getElementById('R_Password').value; //getting value of registering username
+    let Username = document.getElementById('R_Username').value; //getting value of registering password
     if (VerifyPassword(Password) === false) {
-        let V_Password = document.getElementById("V_Password");
+        let V_Password = document.getElementById("V_Password"); //displaying the incorrect password requirements text
         V_Password.style.display = "inline-block";
     }
     VerifyUsername(Username);
     if (VerifyUsername()  === false) {
-        let V_Username = document.getElementById("V_User");
+        let V_Username = document.getElementById("V_User"); //displaying the incorrect username requirement text
         V_Username.style.display = "inline-block";
     }
 }
 
 function VerifyPassword(Password) {
-    let reg = new RegExp("^(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$");
-    return reg.test(Password);
+    let reg = new RegExp("^(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$"); //regular expression pattern checking
+    return reg.test(Password); //boolean value
 }
 
 function VerifyUsername(Username) {}
 
-function Sign_inMenu() {
+function Sign_inMenu() { //displays the sign-in menu
     console.log("sign-in menu");
-    let Sign_inMenu = document.getElementById('Sign-inMenu');
+    let Sign_inMenu = document.getElementById('Sign-inMenu'); //displaying the sign-in menu element
     Sign_inMenu.style.display = "inline-block";
-    let SignIn_RegisterMenu = document.getElementById("Sign-in/Register");
+    let SignIn_RegisterMenu = document.getElementById("Sign-in/Register"); //hiding the register menu element
     SignIn_RegisterMenu.style.display = "none";
 }
 
@@ -50,11 +50,11 @@ function ValidateUser(){
     let incorrect_login = document.getElementById('incorrect_login');
     console.log('ValidateUser');
     let Sign_inMenu = document.getElementById('Sign-inMenu');
-    let S_Username = document.getElementById('S_Username').value;
-    let S_Password = document.getElementById('S_Password').value;
+    let S_Username = document.getElementById('S_Username').value; //getting sign-in username input value
+    let S_Password = document.getElementById('S_Password').value; //getting sign-in password input value
     console.log(S_Username);
     console.log(S_Password);
-    for (let i = 0; i < UserPass.Users.length; i++) {
+    for (let i = 0; i < UserPass.Users.length; i++) { //loops through the javascript object checking if the username matches with the stored password
         if (S_Username === UserPass.Users[i].Username && S_Password === UserPass.Users[i].Password) {
             Sign_inMenu.style.display = "none";
             console.log(i + " index");
@@ -62,12 +62,12 @@ function ValidateUser(){
             break;
         }
     }
-    incorrect_login.style.display = "inline-block";
+    incorrect_login.style.display = "inline-block"; //displays incorrect login text
 }
 
 function DisplayStudyPlanner(S_Username, i) {
     console.log('D_StudyPlanner');
-    let D_StudyPlanner = document.getElementById('DisplayStudyPlanner');
+    let D_StudyPlanner = document.getElementById('DisplayStudyPlanner'); //displaying this element
     D_StudyPlanner.style.display = "inline-block";
     let StudyPlanner = document.getElementById('StudyPlanner');
     let total_periods = (UserPass.Users[i].Subjects).length + (UserPass.Users[i].Assessments).length;
@@ -76,7 +76,7 @@ function DisplayStudyPlanner(S_Username, i) {
     console.log(UserPass.Users[i].Assessments + " Assessments");
     let d = new Date();
     console.log(d + "d");
-    let weekday = new Array(7);
+    let weekday = new Array(7); //setting the day so get when the study periods should start, set by the student
     weekday[0] = 6; //Sun
     weekday[1] = 0; //Mon
     weekday[2] = 1; //Tue
@@ -85,8 +85,9 @@ function DisplayStudyPlanner(S_Username, i) {
     weekday[5] = 4; //Fri
     weekday[6] = 5; //Sat
     let n = weekday[d.getDay()];
-    let s_time = UserPass.Users[i].Time[n];
+    let s_time = UserPass.Users[i].Time[n]; //starting study time from the student
     console.log(i)
+    //for loop adds a row for each subject and the time to study for each subject
     for (let j = 0; j < (UserPass.Users[i].Subjects).length; j++) { //need to change for all (UserPass.Users[i].Subjects).length to all periods
         let row = StudyPlanner.insertRow(j);
         let time = row.insertCell(0);
@@ -98,19 +99,19 @@ function DisplayStudyPlanner(S_Username, i) {
 }
 
 function DisplayAssessmentCalender() {
-    let D_StudyPlanner = document.getElementById('DisplayStudyPlanner');
+    let D_StudyPlanner = document.getElementById('DisplayStudyPlanner'); //hiding all other elements
     D_StudyPlanner.style.display = "none";
     let ACalenderMenu = document.getElementById('ACalenderMenu');
     ACalenderMenu.style.display = "flex";
+    const date = new Date(); //setting current date
     const renderCalender = () => {
-        const date = new Date();
+        const date = new Date(); //gets day
         const lastDay = new Date(date.getFullYear(),date.getMonth() + 1,0).getDate()//gets last date of the current month
-        const firstDayIndex = date.getDay()
-        const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate()
-        const lastDayIndex = new Date(date.getFullYear(),date.getMonth() + 1,0).getDay()
-        const nextDays = 7 - lastDayIndex - 1;
-        date.setDate(1);
-        const months = [
+        const firstDayIndex = date.getDay() //sets the index value for the first day
+        const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate() //gets the last day to display from the month before (not the last date of the month)
+        const lastDayIndex = new Date(date.getFullYear(),date.getMonth() + 1,0).getDay() //index the last day
+        const nextDays = 7 - lastDayIndex - 1; //the next day's after the current month
+        const months = [ //index the months in order
             "January",
             "February",
             "March",
@@ -124,18 +125,16 @@ function DisplayAssessmentCalender() {
             "November",
             "December",
         ];
-        console.log(date);
-        console.log(months);
-        document.querySelector(".date h2").innerHTML = months[date.getMonth()];
-        document.querySelector(".date p").innerHTML = date.toDateString();
+        document.querySelector(".date h2").innerHTML = months[date.getMonth()]; //changes the html tag to the month
+        document.querySelector(".date p").innerHTML = date.toDateString(); //changes the html tag to the date
         let days = "";
-        const monthDays = document.querySelector('.days');
-        for (let x = firstDayIndex; x > 0; x--){ //creating a new div element for previous month dates
+        const monthDays = document.querySelector('.days'); //sets the month days
+        for (let x = firstDayIndex; x > 0; x--){ //creating a new div element for previous month days
             days += `<div class="prev-date>${prevLastDay - x + 1}</div>`;
         }
         for (let i = 1; i <= lastDay; i++) { //loops through to displays each day
-            if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
-                days += `<div class="today">${i}</div>`; //x += 10
+            if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) { //checks if the current date is on the calender is the current date to highlight the current date
+                days += `<div class="today">${i}</div>`; //giving it the css tag so the background of the current date is highlighted
             } else {
                 days += `<div>${i}</div>`; //x += 10
             }
@@ -143,13 +142,13 @@ function DisplayAssessmentCalender() {
         for (let j = 1; j<= nextDays; j++) {
             days += `<div class="next-date">${j}</div>`;
         }
-        monthDays.innerHTML = days;
+        monthDays.innerHTML = days; //displays the days for the calender
     }
-    document.querySelector('.prev').addEventListener('click', () => {
+    document.querySelector('.prev').addEventListener('click', () => { //updates the calender to the previous month when the user clicks on the left arrow
         date.setMonth(date.getMonth() - 1);
         renderCalender();
     });
-    document.querySelector('.next').addEventListener('click', () => {
+    document.querySelector('.next').addEventListener('click', () => { //updates the calender to the next month when the user clicks on the right arrow
         date.setMonth(date.getMonth() + 1);
         renderCalender();
     });
